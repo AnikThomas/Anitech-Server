a4e433a915a065d08e700997bd90a1c7d967e88b
Anik Thomas <aniek.sw@gmail.com>
2021-07-10 08:52:28 -0600

    HTTPS and Secure Communication

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,6 @@ const authenticate = require('../authenticate');
 
 const router = express.Router();
 
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
